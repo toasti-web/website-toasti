6440af17ba58307665bd2460f0b9ad4be6d5d4de
Razita Afrina Nuriyan <razita.afrina@ui.ac.id>
2022-07-09 17:23:45 +0700

    Remove unnecessary dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-transformer-json`,
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `images`,
-      path: `${__dirname}/src/data`,
-    },
-  }],
+  plugins: [],
 }
